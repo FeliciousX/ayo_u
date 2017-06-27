6bc8cdd2c44a569664ca8df554584b5cda4faa32
Churchill Lee <churchill@piktochart.com>
2017-06-27 18:36:24 +0800

    style(src): follow ktlint code standard

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,19 @@
-external fun require(module:String):dynamic
+external fun require(module: String): dynamic
 
 fun main(args: Array<String>) {
-  println("Hello JavaScript!")
+    println("Hello JavaScript!")
 
-  val express = require("express")
-  val app = express()
+    val express = require("express")
+    val app = express()
 
-  app.get("/", { req, res ->
-    res.type("text/plain")
-    res.send(JSON.stringify(req.headers, { key:String, value:Any? -> value }, 2))
-  })
+    app.get("/", { req, res ->
+        val headers = JSON.stringify(req.headers, { key: String, value: Any? -> value }, 2)
+        res.type("text/plain")
+        res.send(headers)
+        println(headers)
+    })
 
-  app.listen(3000, {
-    println("Listening on port 3000")
-  })
+    app.listen(3000, {
+        println("Listening on port 3000")
+    })
 }
