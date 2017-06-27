83261b629ee96e52f64bce321781694524cefda4
Churchill Lee <churchill@piktochart.com>
2017-06-27 17:58:30 +0800

    refactor(): use 2 spaces as indentation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,17 @@
 external fun require(module:String):dynamic
 
 fun main(args: Array<String>) {
-    println("Hello JavaScript!")
+  println("Hello JavaScript!")
 
-    val express = require("express")
-    val app = express()
+  val express = require("express")
+  val app = express()
 
-    app.get("/", { req, res ->
-        res.type("text/plain")
-        res.send("i am a beautiful butterfly")
-    })
+  app.get("/", { req, res ->
+    res.type("text/plain")
+    res.send(JSON.stringify(req.headers, { key:String, value:Any? -> value }, 2))
+  })
 
-    app.listen(3000, {
-        println("Listening on port 3000")
-    })
+  app.listen(3000, {
+    println("Listening on port 3000")
+  })
 }
